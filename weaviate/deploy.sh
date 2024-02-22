b79a78cdb81135ae2847e5cf102877c155794100
etiennedi <etienne@dilocker.de>
2024-02-22 21:04:45 +0100

    run with RAFT image

--- weaviate/deploy.sh
+++ weaviate/deploy.sh
@@ -2,11 +2,14 @@
 
 cd "${0%/*}"
 
-helm repo add weaviate https://weaviate.github.io/weaviate-helm | true
+rm -rf "/tmp/weaviate-helm"
+git clone -b raft-configuration https://github.com/weaviate/weaviate-helm.git "/tmp/weaviate-helm" 
+helm package -d /tmp/weaviate-helm /tmp/weaviate-helm/weaviate 
+
 kubectl create ns "$K8S_NAMESPACE" | true
 
 helm upgrade \
-  --install weaviate-load-test weaviate/weaviate --version 16.8.1 \
+  --install weaviate-load-test /tmp/weaviate-helm/weaviate-*.tgz \
   --values values.yaml \
   --namespace "$K8S_NAMESPACE" \
   --set "image.tag=$WEAVIATE_VERSION" \
