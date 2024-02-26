915c28952ca72508e16561c11a2d5df26ed7cd06
etiennedi <etienne@dilocker.de>
2024-02-26 13:26:20 +0100

    try running no-deadlock preview image

--- cli_config.py
+++ cli_config.py
@@ -105,7 +105,7 @@ def init_config(
 
     # todo: make configurable
     cfg.weaviate_pods = 12
-    cfg.weaviate_version = "1.23.10"
+    cfg.weaviate_version = "preview-prevent-schema-deadlock-split-incomingcommit-into-locked-unlocked-parts-1c6590b"
 
     cfg.replication_factor = 1
     cfg.tenants_per_job = 1000
