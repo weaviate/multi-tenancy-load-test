ad5986530456a94bf35737a6e6b617d0a6fa96cf
etiennedi <etienne@dilocker.de>
2024-04-28 10:43:11 +0200

    fix image name

--- cli_config.py
+++ cli_config.py
@@ -105,7 +105,7 @@ def init_config(
 
     # todo: make all of these configurable from the outside
     cfg.weaviate_pods = 12
-    cfg.weaviate_version = "semitechnologies/weaviate:1.25.0-raft-83a51ad"
+    cfg.weaviate_version = "1.25.0-raft-83a51ad"
 
     cfg.replication_factor = 1
     cfg.tenants_per_job = 5000
